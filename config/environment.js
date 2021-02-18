a6809c104fac1fb51dfb1e5bc3451306d59f4fde
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-18 14:19:26 +0000

    location model

---
+++ config/environment.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const environment = process.env.NODE_ENV || 'development'
+export const dbURI = environment === 'production'
+  ? process.env.MONGODB_URI
+  : `monodb://localhost/pingpongdb-${environment}`
+
+// export const dbURI = 'mongodb://localhost/pingpongdb'
+export const port = 8000
+// export const secret = 'sklghkdflasghkahhl;adfkhl23589sgjk34khk3b5kjh34kbt34jkbhj345jhg34j5hg3hjvbfthj345g3hjbf34hjghj3bfhj3gfhj3gh;rklh'
+export const secret = process.env.SECRET
+
---
+++ models/location.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+import commentSchema from './comment.js'
+
+const locationSchema = new mongoose.Schema({
+  image: { 
+    type: String, 
+    required: true,
+    validate: (image) => typeof image === 'string' && image.length > 0 && image.includes('https:' || 'http:')
+  },
+  location: {
+    lat: { 
+      type: Number,
+      required: true,
+      validate: (lat) => typeof lat === 'number' && (lat >= -90) && (lat <= 90)
+    },
+    long: {
+      type: Number,
+      required: true,
+      validate: (long) => typeof long === 'number' && (long >= -180) && (long <= 180)
+    }
+  },
+  address: { 
+    type: String,
+    required: false,
+    validate: (name) => typeof name === 'string' && name.length > 0 
+  },
+  facilities: {
+    numberOfTables: { 
+      type: Number,
+      required: true,
+      validate: (numberOfTables) => typeof numberOfTables === 'number' && numberOfTables > 0 
+    },
+    description: { 
+      type: String, 
+      required: false,
+      validate: (name) => typeof name === 'string' && name.length > 0 
+    }
+  },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  comments: [ commentSchema ],
+  events: { type: [Object], required: false }
+})
+
+export default mongoose.model('Location', locationSchema)
+
+

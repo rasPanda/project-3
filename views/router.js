3c5b989f19fad8d79a9375ea86d9c27da484ba7e
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-18 15:56:04 +0000

    creating location controller/routes

--- views/router.js
+++ views/router.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import location from '../controllers/locationController.js'
 //! Import all our controllers here
 //! For example
 // import userControllers from '../controllers/userControllers.js'
@@ -12,6 +13,17 @@ const router = express.Router()
 //! For example
 // router.route('/register')
 //   .post(userController.register)
+router.route('/location')
+  .get(location.getAllLocation)
+  .post(location.makeLocation)
+
+router.route('/location/:id')
+  .get(location.getSingleLocation)
+  .put(secureRoute, location.updateLocation)
+  .delegates(secureRoute, location.deleteLocation)
+
+router.route('/location/:name')
+  .get(location.getLocationByName)
 
 
 

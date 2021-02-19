5eaf182c77fc6b59f370fd1324d8e42d02decc7b
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-19 10:27:55 +0000

    user seed data

---
+++ db/data/userData.js
@@ -0,0 +1,74 @@
+export default function getUserData() {
+  return [
+    {
+      username: 'joe',
+      email: 'joe@joe.com',
+      password: 'joe',
+      isAdmin: true,
+      image: 'https://joesface.com',
+      bio: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      location: 'Bristol'
+    },
+    {
+      username: 'yusuf',
+      email: 'yusuf@yusuf.com',
+      password: 'yusuf',
+      isAdmin: true,
+      image: 'https://yusufsface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'Brick Lane'
+    },
+    {
+      username: 'carl',
+      email: 'carl@carl.com',
+      password: 'carl',
+      isAdmin: true,
+      image: 'https://carlsface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'London'
+    },
+    {
+      username: 'james',
+      email: 'james@james.com',
+      password: 'james',
+      isAdmin: true,
+      image: 'https://jamesface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'London'
+    },
+    {
+      username: 'bill',
+      email: 'bill@bill.com',
+      password: 'bill',
+      image: 'https://billsface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'New York'
+    },
+    {
+      username: 'sarah',
+      email: 'sarah@sarah.com',
+      password: 'sarah',
+      image: 'https://sarahsface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'Edinburgh'
+    },
+    {
+      username: 'patrick',
+      email: 'patrick@patrick.com',
+      password: 'patrick',
+      isAdmin: false,
+      image: 'https://patricksface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'Dublin'
+    },
+    {
+      username: 'beth',
+      email: 'beth@beth.com',
+      password: 'beth',
+      isAdmin: false,
+      image: 'https://bethsface.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+      location: 'Kings Lynn'
+    },
+  ]
+}

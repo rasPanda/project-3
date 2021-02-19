e6f7c259a7103bfdc8b5ed6c8c830c810f34a22b
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-19 14:13:38 +0000

    initial scripts for testing users

---
+++ __tests__/lib/tearDown.js
@@ -0,0 +1,9 @@
+import User from '../../models/user.js'
+import Location from '../../models/location.js'
+import Event from '../../models/event.js'
+export default async function tearDown(done) {
+  await User.deleteMany()
+  await Location.deleteMany()
+  await Event.deleteMany()
+  done()
+}

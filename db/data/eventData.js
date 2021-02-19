6740e13cf73046abd5318f379231a2d59028cdee
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-19 10:28:33 +0000

    creating events data fro seeding

---
+++ db/data/eventData.js
@@ -0,0 +1,70 @@
+export default function getEventData(users, location) {
+  return [
+    {
+      name: 'Ball Busters',
+      location: location[0],
+      user: users[0],
+      image: 'https://images.unsplash.com/photo-1557804500-7a58fbcd4d1a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
+      time: ' Sunday 21st March at 3pm',
+      details: 'meeting at the south corner of sweet park',
+      attendees: users[1],
+      results: [],
+      comments: []
+    },
+    {
+      name: 'King Pong',
+      location: location[1],
+      user: users[1],
+      image: 'https://images.unsplash.com/photo-1568711146297-b8674c3c11b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      time: 'Saturday 20th March at 3pm ',
+      details: 'meeting at the near pub,  then walking  to the spot ',
+      attendees: users[4],
+      results: [],
+      comments: []
+    },
+    {
+      name: 'Ping Pongthers',
+      location: location[2],
+      user: users[2],
+      image: 'https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      time: 'Tuesday 24th March 6:30pm',
+      details: 'meeting at the main gate, buying drink and start palying',
+      attendees: users[0],
+      results: [],
+      comments: []
+    },
+    {
+      name: 'Total Pongage',
+      location: location[3],
+      user: users[3],
+      image: 'https://images.unsplash.com/photo-1576617497557-22895ee5930b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      time: ' Thursday 18th March at 8pm ',
+      details: 'meeting at the main gate, buying drink and start palying',
+      attendees: users[2],
+      results: [],
+      comments: []
+    },
+    {
+      name: 'Miss Hits',
+      location: location[4],
+      user: users[5],
+      image: 'https://images.unsplash.com/photo-1572025442937-d55974d9815f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1231&q=80',
+      time: 'Thursday 18th March at 8pm',
+      details: 'meeting at the main gate, buying drink and start palying',
+      attendees: users[3],
+      results: [],
+      comments: []
+    },
+    {
+      name: location[5],
+      location: 'Green park, London',
+      user: users[5],
+      image: 'https://images.unsplash.com/photo-1559077184-562609f0a10b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80',
+      time: 'Saturday 27th March at 3pm',
+      details: 'meeting at the near pub,  then walking  to the spot',
+      attendees: users[2],
+      results: [],
+      comments: []
+    }
+  ]
+}

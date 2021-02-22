4f4d728801f3512926da2b2db3a38b66ed6cfbbe
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-22 10:17:31 +0000

    fixing  bug to run test

--- __tests__/event_test.js
+++ __tests__/event_test.js
@@ -14,7 +14,7 @@ describe('Testing GET Event', () => {
   })
 
   it('should return a 200 response', done => {
-    api.get('/api/events')
+    api.get('/api/event')
       .end((err, res) => {
         expect(res.status).to.eq(200)
         done()
@@ -22,7 +22,7 @@ describe('Testing GET Event', () => {
   })
 
   it('should return an array of 6 Event', done => {
-    api.get('/api/events')
+    api.get('/api/event')
       .end((err, res) => {
         expect(res.body).to.be.an('array')
         expect(res.body.length).to.eq(6)
@@ -32,29 +32,52 @@ describe('Testing GET Event', () => {
 
 
   it('should return an array of 1 Event', done => {
-    api.post('/api/events')
-      .set('Authorization', `Bearer ${res.body.token}`)
+    api.post('/api/login')
       .send({
-        name: 'King test',
-        location: location[1],
-        user: users[1],
-        image: 'https://images.unsplash.com/photo-1568711146297-b8674c3c11b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        time: 'Saturday 20th March at 3pm',
-        details: 'meeting at the near pub,  then walking  to the spot ',
-        attendees: [users[3], users[4], users[5]],
-        results: [{}],
-        comments: []
+        email: 'joe@joe.com',
+        password: 'joe'
       })
       .end((err, res) => {
-        expect(res.body).to.be.a('object')
-        expect(res.body.length).to.eq(1)
-        expect(res.status).to.eq(201)
-        api.delete(`/api/events/${res.body._id}`)
-          .set('Bearer', `${res.body.token}`)
+        expect(res.status).to.eq(202)
+
+
+        api.post('/api/event')
+          .set('Authorization', `Bearer ${res.body.token}`)
+          .send(
+            {
+              name: 'King Pong',
+              location: {
+                lat: 51.483857,
+                long: -0.084035
+              },
+              user: {
+                username: 'peter',
+                email: 'peter@peter.com',
+                password: 'peter',
+                passwordConfirmation: 'peter',
+                image: 'petersFace.com',
+                bio: 'Lorem ipsum, caecalius est in horto',
+                location: 'A city somewhere nowhere'
+              }
+              ,
+              image: 'https://images.unsplash.com/photo-1568711146297-b8674c3c11b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+              time: 'Saturday 20th March at 3pm',
+              details: 'meeting at the near pub,  then walking  to the spot ',
+              attendees: [],
+              results: [{}],
+              comments: []
+            }
+          )
           .end((err, res) => {
             expect(res.body).to.be.a('object')
-            expect(res.body.length).to.eq(1)
-            done()
+            // done()
+
+            api.delete(`/api/event/${res.body._id}`)
+              .set('Bearer', `${res.body.token}`)
+              .end((err, res) => {
+                expect(res.body).to.be.a('object')
+                done()
+              })
           })
       })
   })
--- __tests__/location_test.js
+++ __tests__/location_test.js
@@ -1,7 +1,7 @@
 // /* eslint-disable no-undef */
-// import { expect } from 'chai'
-// import setup from './lib/setup.js'
-// import tearDown from './lib/tearDown.js'
+import { expect } from 'chai'
+import setup from './lib/setup.js'
+import tearDown from './lib/tearDown.js'
 
 describe('Testing locations', () => {
 

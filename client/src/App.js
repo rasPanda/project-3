acbdafa98dd51487f675ad2d2df3afd0654db171
Joseph Schenk <jcschenk8@gmail.com>
2021-02-22 10:32:01 +0000

    app.js etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+//! Import Components here
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+
+
+import 'bulma'
+import './styles/style.scss'
 
 const App = () => (
-  <h1>Hello world</h1>
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home}/>
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App

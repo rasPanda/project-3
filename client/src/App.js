d50352134b9bfcaa9ba011147e19fd1690188bde
raspanda <carlviberg@gmail.com>
2021-02-22 16:24:29 +0000

    events details

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 //! Import Components here
-// import Navbar from './components/Navbar'
-// import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import EventsPage from './components/Events'
+import SingleEventPage from './components/EventDetails'
 import Users from './components/Users'
 import SingleUser from './components/SingleUser'
 
@@ -15,15 +16,15 @@ import './styles/style.scss'
 
 const App = () => (
   <BrowserRouter>
-    {/* <Navbar /> */}
+    <Navbar />
     <Switch>
-      {/* <Route exact path="/" component={Home}/> */}
+      <Route exact path="/" component={Home}/>
       <Route exact path="/events" component={EventsPage}/>
+      <Route exact path="/event/:id" component={SingleEventPage}/>
       <Route exact path="/users" component={Users}/>
       <Route exact path="/user/:id" component={SingleUser}/>
       
       <Route exact path="/login" component={LoginAndRegister}/>
-      {/* <Route exact path="/" component={Home} /> */}
     </Switch>
   </BrowserRouter>
 )

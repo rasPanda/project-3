a089dc2ad53fe17c99af70a7c3966a3c4c717a6f
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-23 11:31:30 +0000

    users update

---
+++ client/src/components/UserUpdate.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function UserUpdateForm({ formData, handleSave, handleChange }) {
+  const inputFields = ['username', 'location']
+
+  return <form className="column">
+    {inputFields.map(field => {
+    return <div key={field} className="field">
+        <label className="label">
+          {field[0].toUpperCase() + field.slice(1)}
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            value={formData[field]}
+            onChange={handleChange}
+            name={field}
+          />
+        </div>
+      </div>
+    })}
+    <label className="label">Bio</label>
+    <div className="control">
+      <textarea
+        className="input"
+        type="text"
+        value={formData.bio}
+        onChange={handleChange}
+        name='bio'
+      />
+    </div>
+    <button className="button mt-5 is-success" onClick={handleSave}>Save</button>
+  </form>
+}

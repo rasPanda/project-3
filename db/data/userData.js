f73b7d4f95fa78f597fac4b013cf619a65098008
Joseph Schenk <jcschenk8@gmail.com>
2021-02-25 16:34:04 +0000

    updated seed data

--- db/data/userData.js
+++ db/data/userData.js
@@ -1,77 +1,77 @@
 export default function getUserData() {
   return [
     {
-      username: 'joe',
+      username: 'Joe',
       email: 'joe@joe.com',
       password: 'joe',
       passwordConfirmation: 'joe',
       isAdmin: true,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614266117/gbchafvomcrnw4dah5ol.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Hi I\'m Joe! I served up code in the ace that is Park Pong! Top skin skills to match my JavaScript, game on!!!',
       location: 'Bristol'
     },
     {
-      username: 'yusuf',
+      username: 'Yusuf',
       email: 'yusuf@yusuf.com',
       password: 'yusuf',
       passwordConfirmation: 'yusuf',
       isAdmin: true,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614268358/voybc7c4z9mbhyiqmah5.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'My name is Yusuf, what I lack in ping pong skills I make up for as a legendary software engineer! Working on Park Pong was almost as much fun as playing Ping Pong every day since!',
       location: 'Brick Lane'
     },
     {
-      username: 'carl',
+      username: 'Carl',
       email: 'carl@carl.com',
       password: 'carl',
       passwordConfirmation: 'carl',
       isAdmin: true,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614265953/zvhgxmiijvrzxvrrukhx.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'I\'m Carl, table tennis master and part of the genius behind Park Pong! I\'ve always said there aren\'t enough websites for setting up games of Ping Pong but not anymore!',
       location: 'London'
     },
     {
-      username: 'james',
+      username: 'James',
       email: 'james@james.com',
       password: 'james',
       passwordConfirmation: 'james',
       isAdmin: true,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614100572/vgyscotbynmunjau476q.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'James here, Software Engineer and Park Pong entrepeneur. I hold the unofficial record for fastest serve in Wimbledon [Ping Pong Park]. Some say the ping pong ball reached supersonic speeds... Set up a meet and you can see for yourself!' ,
       location: 'London'
     },
     {
-      username: 'bill',
+      username: 'Bill',
       email: 'bill@bill.com',
       password: 'bill',
       passwordConfirmation: 'bill',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614180366/xz5bru78okkincrortuf.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Bill \'The Serve\' Paxton, as they call me in Hollywood. Psyched about this rad website, bring it on!!!',
       location: 'New York'
     },
     {
-      username: 'sarah',
+      username: 'Sarah',
       email: 'sarah@sarah.com',
       password: 'sarah',
       passwordConfirmation: 'sarah',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614182131/tdraljhmktepxi6antwg.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'When I\'m not saving the world from The Terminator I\'m playing Ping Pong in Edinburgh. Arny once said "I\'ll be back...handing your serve", but he was wrong, I aced him!',
       location: 'Edinburgh'
     },
     {
-      username: 'patrick',
+      username: 'Patrick',
       email: 'patrick@patrick.com',
       password: 'patrick',
       passwordConfirmation: 'patrick',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614182297/oyn2y1hjval6mkrb49ow.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Dublin based master of Ping Pong, my friends call me \'Paddle\'! If you can beat me the Guinness is on the house!',
       location: 'Dublin'
     },
     {
-      username: 'beth',
+      username: 'Beth',
       email: 'beth@beth.com',
       password: 'beth',
       passwordConfirmation: 'beth',
@@ -81,7 +81,7 @@ export default function getUserData() {
       location: 'Kings Lynn'
     },
     {
-      username: 'simon',
+      username: 'Simon',
       email: 'simon@simon.com',
       password: 'simon',
       passwordConfirmation: 'simon',
@@ -91,7 +91,7 @@ export default function getUserData() {
       location: 'Boston'
     },
     {
-      username: 'jasper',
+      username: 'Jasper',
       email: 'jasper@jasper.com',
       password: 'jasper',
       passwordConfirmation: 'jasper',
@@ -101,7 +101,7 @@ export default function getUserData() {
       location: 'Henley'
     },
     {
-      username: 'frederick',
+      username: 'Frederick',
       email: 'frederick@frederick.com',
       password: 'frederick',
       passwordConfirmation: 'frederick',
@@ -111,7 +111,7 @@ export default function getUserData() {
       location: 'Santa Monica'
     },
     {
-      username: 'ada',
+      username: 'Ada',
       email: 'ada@ada.com',
       password: 'ada',
       passwordConfirmation: 'ada',
@@ -121,7 +121,7 @@ export default function getUserData() {
       location: 'Galway'
     },
     {
-      username: 'patricia',
+      username: 'Patricia',
       email: 'patricia@patricia.com',
       password: 'patricia',
       passwordConfirmation: 'patricia',
@@ -131,7 +131,7 @@ export default function getUserData() {
       location: 'Stafford'
     },
     {
-      username: 'paul',
+      username: 'Paul',
       email: 'paul@paul.com',
       password: 'paul',
       passwordConfirmation: 'paul',

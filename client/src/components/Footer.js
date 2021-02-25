dc886d76f94db75162cf1bb2e2bb43bd79bed578
raspanda <carlviberg@gmail.com>
2021-02-25 17:58:31 +0000

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer-container'>
-      &copy;  Designed, Developed and Powered by Carl Viberg, James Ryan, Joseph Schenk and Yusuf 
+      &copy;  Designed, Developed and Powered by Carl Viberg, James Ryan, Joseph Schenk and Yusuf Musa
     </footer>
   )
 }

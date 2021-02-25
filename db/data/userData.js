3a69068840ce551daf483b2861fd1a17b187ab6a
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-25 16:35:09 +0000

    user descriptions

--- db/data/userData.js
+++ db/data/userData.js
@@ -71,74 +71,76 @@ export default function getUserData() {
       location: 'Dublin'
     },
     {
-      username: 'beth',
+      username: 'Beth Tudor',
       email: 'beth@beth.com',
       password: 'beth',
       passwordConfirmation: 'beth',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614183418/xfcmmelww9tmcmtv4hfl.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      location: 'Kings Lynn'
+      bio: 'Park Pong is my time to relax and let myself go! You can find me in any park around Hampton Court. I love playing under the shoadow of the palace.',
+      location: 'Hampton'
     },
     {
-      username: 'simon',
+      username: 'Simon Pegg',
       email: 'simon@simon.com',
       password: 'simon',
       passwordConfirmation: 'simon',
       isAdmin: false,
       image: 'https://i.pinimg.com/564x/f2/b6/e4/f2b6e41cda9aed63ecbcd32de69b825a.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Hit me up for some matches. Defending Cambridge (the one in the US) regional champion at Park Pong. I coach too, if interested: 077495201578',
       location: 'Boston'
     },
     {
-      username: 'jasper',
+      username: 'Jasper Casper',
       email: 'jasper@jasper.com',
       password: 'jasper',
       passwordConfirmation: 'jasper',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614183574/hmqpmeaat62ryx604ut4.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'When I\'m not playing Polo, you can find me hitting some aces down the local park! I also love rowing, so if you like any of that then we will definitely get along..!',
       location: 'Henley'
     },
     {
-      username: 'frederick',
+      username: 'Frederick',
       email: 'frederick@frederick.com',
       password: 'frederick',
       passwordConfirmation: 'frederick',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614184015/xzdxtovvypmzpnbypnla.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'What\'s up ya\'ll! If anyone wants to get together and play a few rounds of Park Pong in Santa Monica, hit me up.',
       location: 'Santa Monica'
     },
     {
-      username: 'ada',
+      username: 'Ada Selby',
       email: 'ada@ada.com',
       password: 'ada',
       passwordConfirmation: 'ada',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614184015/xzdxtovvypmzpnbypnla.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Hi I\'m Ada from Galway. I spent a few years in Birmingham and I loved play ping pong there. Lots of parks to play the greatest game ever!',
       location: 'Galway'
     },
     {
-      username: 'patricia',
+      username: 'Patricia Sanders',
       email: 'patricia@patricia.com',
       password: 'patricia',
       passwordConfirmation: 'patricia',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614266551/b1eykdj0wwu31xlpwub6.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: `Hi I\'m Patrcicia. I have 5 kids, who love playing table tennis. 
+            My favourite time of the week is that precious hour on a friday, when I head down to the local 
+            park and play table tennis with the other local mums. Mums rule!`,
       location: 'Stafford'
     },
     {
-      username: 'paul',
+      username: 'Paul Simon',
       email: 'paul@paul.com',
       password: 'paul',
       passwordConfirmation: 'paul',
       isAdmin: false,
       image: 'https://res.cloudinary.com/dzoqli241/image/upload/v1614268437/bjssyjzk1eiendjhutxz.jpg',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      location: 'Vienna'
+      bio: 'Hi I\'m Paul, but you can call me Al. I love Ping Pong/Table tennis/mini ball game! I have always loved playing in and around London!',
+      location: 'Graceland'
     }
   ]
 }

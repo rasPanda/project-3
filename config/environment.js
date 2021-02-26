ac4852ff9390121506826b2cd993050968d2f8c0
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-26 09:43:56 +0000

    adding send mailer

--- config/environment.js
+++ config/environment.js
@@ -11,3 +11,4 @@ export const port = 8000
 // export const secret = 'sklghkdflasghkahhl;adfkhl23589sgjk34khk3b5kjh34kbt34jkbhj345jhg34j5hg3hjvbfthj345g3hjbf34hjghj3bfhj3gfhj3gh;rklh'
 export const secret = process.env.SECRET
 export const MapboxAccessToken = process.env.MAPBOX_TOKEN
+export const API_KEY = process.env.API_KEY || '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'
--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,13 @@
 import User from '../models/user.js'
+import sendgridTransport from 'nodemailer-sendgrid-transport'
+import nodeMailer from 'nodemailer'
+import { API_KEY } from '../config/environment.js'
+
+const transporter = nodeMailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: API_KEY
+  }
+}))
 
 async function register(req, res, next) {
   if (req.body.isAdmin) {
@@ -7,7 +16,15 @@ async function register(req, res, next) {
   const body = req.body
   try {
     const user = await User.create(body)
-    res.status(201).send(user)
+    res.send(
+      transporter.sendMail({
+        to: body.email,
+        from: 'parkpong63@gmail.com',
+        subject: 'Registeration successful',
+        html: `<h2>Dear ${body.username},</h2> <br><h3> thank your for your registration with us.</h3>
+        <img width="100vw" src="https://images.unsplash.com/photo-1611251126118-b1d4f99600a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"/>
+        `
+      }))
     console.log('new user')
   } catch (err) {
     next(err)

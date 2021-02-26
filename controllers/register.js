67c787ad2fb4ab154622f20ce58f18a79fbde10a
Joseph Schenk <jcschenk8@gmail.com>
2021-02-26 11:01:20 +0000

    deployment

--- controllers/register.js
+++ controllers/register.js
@@ -21,7 +21,7 @@ async function register(req, res, next) {
         to: body.email,
         from: 'parkpong63@gmail.com',
         subject: 'Registeration successful',
-        html: `<h2>Dear ${body.username},</h2> <br><h3> thank your for your registration with us.</h3>
+        html: `<h2>Dear ${body.username},</h2> <br><h3> Thank you for your registration with us.</h3>
         <img width="100vw" src="https://images.unsplash.com/photo-1611251126118-b1d4f99600a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"/>
         `
       }))
